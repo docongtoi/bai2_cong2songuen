a0301fdf2d1c6e77c60ba0ac1b91f24bdda60bc3
docongtoi <docongtoi17a@gmail.com>
2024-12-24 10:29:58 +0700

    Add project files.

---
+++ bai2_cong2songuen.cpp
@@ -0,0 +1,29 @@
+// bai2_cong2songuen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ /*
+    int so1 = 123;
+    int so2 = 234;
+    int tong = so1 + so2;
+    cout << "tong 2 so tren la: " << tong;
+
+  */
+    double so1 , so2;
+
+    // nhan gia tri vao bien 
+    cout << "nhap so 1";
+    cin >> so1;
+    cout << "nhap so 2";
+    cin >> so2;
+
+    double tong = so1 + so2;
+    cout << "tong 2 so tren la" << tong;
+
+}  
+
+
+
